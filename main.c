5105ba817661da42f173d056e5dc02f72b2592a6
crazyguevaraa <70381198+crazyguevaraa@users.noreply.github.com>
2020-10-20 21:48:48 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,78 +5,64 @@
 void PrintArray (char* array, off_t size);
 int main()
 {
-    int   nVal = 1;
-    struct lenBuf
-    {
-        int   len;
-        char* pt;
-    };
-    struct lenBuf* arr = (struct lenBuf*) calloc((nVal), sizeof(*arr));
+    int nVal = 2;
     struct stat statistica = {0};
     int stat_error = stat ("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", &statistica);
     assert(stat_error == 0);
     printf ("%lld\n", statistica.st_size);
-    FILE *input_file = fopen("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", "rb");
-    for (int i = 0; i < statistica.st_size + 1 ; i++)
-    {
-        int symbol = fgetc(input_file);
-        printf ("symbol (%c) code (%d);", symbol, symbol);
-    }
-    fclose (input_file);
-
+    //long long int size = statistica.st_size;
+    FILE *input_file = fopen("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", "r");
     printf("\n\n\n");
-    char* buffer = (char*) calloc ((nVal), sizeof(*buffer));
+    char* buffer = (char*) calloc ((statistica.st_size), sizeof(char));
     if (input_file == NULL)
     {
         printf("\n ERROR\n");
         return -1;
     }
     else printf ("\nCOMPLETE\n");
-    int i = 0;
-    for(;;)
+    int val = 1;
+    char* ptr = (char*) calloc (nVal, sizeof(char));
+    //ptr[0] = *baffer;
+    fread((void*) buffer, sizeof(char), statistica.st_size, input_file);
+    ptr[0] = *buffer;
+   for(int i = 0, n = 1; i < statistica.st_size;)
     {
-        arr->len = fread(*buffer, sizeof(char), statistica.st_size, input_file);
-        struct lenBuf* newArr = (struct lenBuf*) realloc (arr, nVal * sizeof (*arr));
-        if (newArr == NULL)
+        if (buffer[i] == '\n')
         {
-            printf("\nERROR arr\n");
-            break;
+            val++;
+            ptr[n] = buffer[i+1];
+            n++;
+            i++;
+            nVal++;
+            char* NewPtr = (char*) realloc(ptr, nVal * sizeof(char));
+            if (NewPtr == NULL)
+            {
+                printf("\nERROR\n");
+                break;
+            }
+            ptr = NewPtr;
         }
-        if (arr != newArr)
+        else
         {
-            printf("\nWELL DONE arr\n");
+            i++;
         }
-        arr = newArr;
-               
-        if (sscanf (buffer, "%s", &buffer[nVal-1]) != 1)
-        break;
-
-        nVal++; //Сдвиг на 1 ячейку.
-        
-        char* newBuffer = (char*) realloc (buffer, nVal * sizeof (char));
-        if (newBuffer == NULL)
-        {
-            printf("\nERROR\n");
-            break;
-        }
-        if (buffer != newBuffer)
-        {
-            printf("\nWELL DONE\n");
-        }
-        buffer = newBuffer;
-        
     }
-    printf("%lld\n", sizeof(arr));
-    printf("%lu\n", sizeof(struct lenBuf*));
+  
     PrintArray(buffer, statistica.st_size );
+    PrintArray(ptr, val);
+    printf("%lld", val);
     
 }
-void PrintArray (char* array, off_t size ) //Функция печати массива
+void PrintArray (char* array, off_t size )
    {
-   for (int i = 0 ; i < size + 1 ; i++)
+   for (int i = 0 ; i < size; i++)
    {
        printf("[%d] = %c , code (%d)\n", i, array[i], array[i]);
-      // printf("END\n");
    }
     printf("END\n");
    }
+
+char GaySort (char* arr_1, char* arr_2, unsigned long long int size_arr_1)
+{
+    
+}
