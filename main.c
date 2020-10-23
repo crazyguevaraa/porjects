e1aa6f4bd6182b4fbd6a64aa96c3b66d2cff4ba8
crazyguevaraa <70381198+crazyguevaraa@users.noreply.github.com>
2020-10-23 14:31:50 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,30 +2,38 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <assert.h>
-void PrintArray (char* array, off_t size);
+
+void gay_print_array(char* array, off_t size);
+int  gay_compar_str(const char  *str_1, const char *str_2);
+void gay_sort(char* arr_1, unsigned long long int size_arr_1);
+void gay_swap(char* ptr_1, char* ptr_2);
+
 int main()
 {
-    int nVal = 2;
+    int         nVal       = 2;
     struct stat statistica = {0};
-    int stat_error = stat ("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", &statistica);
+    int         stat_error = stat ("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", &statistica);
+   
     assert(stat_error == 0);
     printf ("%lld\n", statistica.st_size);
-    //long long int size = statistica.st_size;
-    FILE *input_file = fopen("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", "r");
-    printf("\n\n\n");
-    char* buffer = (char*) calloc ((statistica.st_size), sizeof(char));
+    
+    FILE  *input_file = fopen("/Users/danilapuchkov/Proga/onegin2.0/onegin2.0/text_1.txt", "r");
+    char* buffer      = (char*) calloc ((statistica.st_size), sizeof(char));
+    
     if (input_file == NULL)
     {
         printf("\n ERROR\n");
         return -1;
     }
     else printf ("\nCOMPLETE\n");
-    int val = 1;
-    char* ptr = (char*) calloc (nVal, sizeof(char));
-    //ptr[0] = *baffer;
+    
+    unsigned long long int val = 1;
+    char*                  ptr = (char*) calloc (nVal, sizeof(char));
+    
     fread((void*) buffer, sizeof(char), statistica.st_size, input_file);
     ptr[0] = *buffer;
-   for(int i = 0, n = 1; i < statistica.st_size;)
+    
+    for(int i = 0, n = 1; i < statistica.st_size;)
     {
         if (buffer[i] == '\n')
         {
@@ -42,27 +50,76 @@ int main()
             }
             ptr = NewPtr;
         }
-        else
-        {
-            i++;
-        }
+        else i++;
     }
-  
-    PrintArray(buffer, statistica.st_size );
-    PrintArray(ptr, val);
-    printf("%lld", val);
+   // char* arr = (char*) calloc (val, sizeof(char*));
     
+    gay_print_array(buffer, statistica.st_size );
+    gay_print_array(ptr, val);
+    gay_sort(ptr, val);
+    printf("%lld\n\n\n", val);
+    gay_print_array(ptr, val);
 }
-void PrintArray (char* array, off_t size )
-   {
+//---------------------------------------------------
+void gay_print_array(char* array, off_t size )
+{
    for (int i = 0 ; i < size; i++)
    {
        printf("[%d] = %c , code (%d)\n", i, array[i], array[i]);
    }
     printf("END\n");
-   }
-
-char GaySort (char* arr_1, char* arr_2, unsigned long long int size_arr_1)
+}
+//---------------------------------------------------
+int gay_compar_str(const char  *str_1, const char *str_2)
 {
+    const unsigned char *name_1 = (const unsigned char*) str_1;
+    const unsigned char *name_2 = (const unsigned char*) str_2;
+   /*int i = 0;
+    if((str_1) >= (str_2))
+    {
+       i = 1;
+    }
+    else if(str_1 <= str_2);
+    else
+    {
+        str_1++;
+        str_2++;
+        gay_compar_str(str_1, str_2);
+    }
+    return i;*/
+    while (*name_1 == *name_2 && *name_1 != '\0' && *name_2 != '\0')
+    {
+      name_1++;
+      name_2++;
+    }
+    return (*name_1 > *name_2) - (*name_1 < *name_2);
+}
+//----------------------------------------------------
+void gay_swap(char* ptr_1, char* ptr_2)
+{
+    int swap_const = *ptr_1;
+            *ptr_1 = *ptr_2;
+            *ptr_2 = swap_const;
+}
+//----------------------------------------------------
+void gay_sort(char* arr_1, unsigned long long int size_arr_1)
+{
+    unsigned long long int i         = size_arr_1 / 2;
+    char                   ptr_left  = arr_1[1];
+    char                   ptr_right = arr_1[size_arr_1];
+    char                   ptr_pivot = arr_1[i];
     
+    for(unsigned long long int k = 0, j = size_arr_1 - 1; k < j; i++, j--)
+    {
+        while(gay_compar_str(ptr_left, ptr_pivot))
+            ptr_left++;
+        while(gay_compar_str(ptr_pivot, ptr_right))
+            ptr_right--;
+        
+        gay_swap(ptr_left, ptr_right);
+        //gay_sort(arr_1, i);
+        //gay_sort(arr_1 + i, size_arr_1 - i);
+    }
+    gay_sort(arr_1, i);
+    gay_sort(arr_1 + i, size_arr_1 - i);
 }
